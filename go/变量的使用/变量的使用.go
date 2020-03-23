3374bba505cfe7d1cfac11ccded272954aee705b
hujiahan <1716867280@qq.com>
2020-03-24 00:43:41 +0800

    提交基本变量使用

---
+++ go/变量的使用/变量的使用.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt" //导入包后必须要使用，不使用会报错
+
+func main() { //左括号必须和函数名同行
+
+	//1、声明变量  var 变量 类型   ,变量声明了必须要使用
+	//2、可以同时声明多个变量 var b,c int
+
+	var a int = 10
+	fmt.Print(a)
+
+}
